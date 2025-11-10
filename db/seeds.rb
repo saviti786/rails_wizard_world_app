cef16e7d0905c791bc9663d07f35a27f5cbe6852
saviti786 <saviti@academic.rrc.ca>
2025-11-10 13:05:32 -0600

    Import API data done!

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,34 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'net/http'
+require 'json'
+
+# Fetch Spells
+puts "Fetching Spells..."
+spell_url = URI("https://wizard-world-api.herokuapp.com/Spells")
+spells = JSON.parse(Net::HTTP.get(spell_url))
+spells.each do |s|
+  Spell.create!(
+    name: s["name"],
+    incantation: s["incantation"],
+    effect: s["effect"],
+    spell_type: s["type"],
+    light: s["light"]
+  )
+end
+
+# Fetch Elixirs
+puts "Fetching Elixirs..."
+elixir_url = URI("https://wizard-world-api.herokuapp.com/Elixirs")
+elixirs = JSON.parse(Net::HTTP.get(elixir_url))
+elixirs.each do |e|
+  Elixir.create!(
+  name: e["name"],
+  effect: e["effect"],
+  difficulty: e["difficulty"],
+  spell: Spell.all.sample  # randomly assign a spell
+)
+end
+
+puts "Done seeding!"
